c354d953e56d32ea51053d8bc120d9eb45c6b7a4
Henna Vahatiitto <hnnamv@gmail.com>
2020-01-10 14:11:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,8 +11,8 @@ class School
   def add_student (name, grade)
     @student_name = name
     @grade = grade
-    @roster [name] = []
-    @roster[name] << grade
+    @roster ["#{name}"] = []
+    @roster["#{name}"] << "#{grade}
   end 
   
 end
